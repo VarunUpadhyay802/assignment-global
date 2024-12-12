4b147b9d9ed21dfd65870d9ce3b42690e319cf97
Varun Upadhyay <varun.802.vu@gmail.com>
2024-12-12 22:55:13 +0530

    usermodel

---
+++ src/userModel.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { updateUser } from './utils/api';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function UserEditModal({ user, onClose, onUpdate }) {
+ 
+  return (
+    <div >
+     
+    </div>
+  );
+}
+
+export default UserEditModal;
